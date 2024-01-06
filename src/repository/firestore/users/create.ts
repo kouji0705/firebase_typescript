6c860b2424ddb7e3695069c285136db298c1e9cc
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-01-06 18:47:27 +0900

    wip

--- src/repository/firestore/users/create.ts
+++ src/repository/firestore/users/create.ts
@@ -2,15 +2,13 @@ import { addDoc, collection, doc, writeBatch } from 'firebase/firestore';
 import { deleteApp } from 'firebase/app';
 import { db } from '..';
 import { USERS_DOCUMENT_NAME } from '../constants/document';
+import { IUser } from '../../../domain/users/model';
 
 // 新しいユーザーを作成する
-async function createNewUser() {
+async function createNewUser(user: IUser) {
   try {
     // `users` コレクションに新しいドキュメントを追加
-    const docRef = await addDoc(collection(db, USERS_DOCUMENT_NAME), {
-      name: 'New User',
-      email: 'newuser@example.com',
-    });
+    const docRef = await addDoc(collection(db, USERS_DOCUMENT_NAME), user);
 
     console.log('ドキュメント作成成功！ドキュメントID: ', docRef.id);
   } catch (e) {
--- src/repository/firestore/users/update.ts
+++ src/repository/firestore/users/update.ts
@@ -1,14 +1,13 @@
 import { doc, updateDoc } from 'firebase/firestore';
 import { db, app } from '..';
 import { USERS_DOCUMENT_NAME } from '../constants/document';
+import { IUser } from '../../../domain/users/model';
 
-async function updateUser(docId: string) {
+async function updateUser(docId: string, user: IUser) {
   const docRef = doc(db, USERS_DOCUMENT_NAME, docId);
 
   try {
-    await updateDoc(docRef, {
-      name: '更新されたユーザー名',
-    });
+    await updateDoc(docRef, user);
 
     console.log('ドキュメント更新成功！');
   } catch (e) {
